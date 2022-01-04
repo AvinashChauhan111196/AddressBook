bc9f2f47c51eb21a3c206387fb4a6c6252ad3acb
Avinash <Avichauhan111196@gmail.com>
2022-01-04 13:24:38 +0530

    Added : Contact field added

---
+++ src/com/address/Address.java
@@ -0,0 +1,22 @@
+package com.address;
+
+public class Address {
+
+    String firstName;
+    String lastName;
+    String city;
+    String address;
+    String email;
+    long mobile;
+    int zip;
+    Address(String firstName,String lastName,String city,String address,String email,long mobile,int zip)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.city=city;
+        this.address=address;
+        this.email=email;
+        this.mobile=mobile;
+        this.zip=zip;
+    }
+}
