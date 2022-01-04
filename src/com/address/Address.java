0528f86633c8c1d24cd351839d432b445c610ac4
Avinash <Avichauhan111196@gmail.com>
2022-01-04 14:15:41 +0530

    Added : Address  edit

--- src/com/address/Address.java
+++ src/com/address/Address.java
@@ -19,4 +19,32 @@ public class Address {
         this.mobile=mobile;
         this.zip=zip;
     }
+    public void setFirstName(String  firstName)
+    {
+        this.firstName=firstName;
+    }
+    public void setLastName(String  lastName)
+    {
+        this.lastName=lastName;
+    }
+    public void setAddress(String address)
+    {
+        this.address=address;
+    }
+    public void setCity(String city)
+    {
+        this.city=city;
+    }
+    public void setEmail(String email)
+    {
+        this.email=email;
+    }
+    public  void  setMobile(String mobile)
+    {
+        this.mobile=mobile;
+    }
+    public void  setZip(int zip)
+    {
+        this.zip=zip;
+    }
 }
--- src/com/address/AddressList.java
+++ src/com/address/AddressList.java
@@ -5,52 +5,126 @@ import java.util.Scanner;
 
 public class AddressList {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Address> list = new ArrayList<>();
-        String lastName, address, email, city, mobile, firstName, search;
-        int zip;
-        int choice = sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Address> list=new ArrayList<>();
+        int choice,zip,editField;
+        String lastName,address,email,city,mobile,firstName,search,fullName;
         do {
             System.out.println(" 1 - Add Contacts");
             System.out.println(" 2 - Edit Contacts");
             System.out.println(" 3 - Delete");
-            System.out.println(" 4 - Display");
-            System.out.println(" 5 - Exit");
-            switch (choice) {
+            System.out.println(" 4 - Exit");
+            choice=sc.nextInt();
+            switch (choice)
+            {
                 case 1:
                     sc.nextLine();
                     System.out.println("Enter the FirstName");
-                    firstName = sc.nextLine();
+                    firstName=sc.nextLine();
                     System.out.println("Enter the LastName");
-                    lastName = sc.nextLine();
+                    lastName=sc.nextLine();
                     System.out.println("Enter the Address");
-                    address = sc.nextLine();
+                    address=sc.nextLine();
                     System.out.println("Enter the City");
-                    city = sc.nextLine();
+                    city=sc.nextLine();
                     System.out.println("Enter the Email");
-                    email = sc.nextLine();
+                    email=sc.nextLine();
                     System.out.println("Enter the Mobile");
-                    mobile = sc.nextLine();
-                    System.out.println("Enter the Pincode");
-                    zip = sc.nextInt();
-                    Address a1 = new Address(firstName, lastName, city, address, email, mobile, zip);
+                    mobile=sc.nextLine();
+                    System.out.println("Enter the Zip");
+                    zip=sc.nextInt();
+                    Address a1=new Address(firstName,lastName,city,address,email,mobile,zip);
                     list.add(a1);
+                    System.out.println(" List size "+list.size());
                     break;
                 case 2:
                     sc.nextLine();
-                    System.out.println("Enter field you want to edit");
-                    search = sc.nextLine();
-                    for (int i = 0; i < list.size(); i++) {
-                        if (list.get(i).firstName.equals(search)) {
-                            System.out.println("done");
+                    System.out.println("Enter first name to edit");
+                    search=sc.nextLine();
+                    for(int i=0;i< list.size();i++)
+                    {
+                        if (list.get(i).firstName.equals(search))
+                        {
+                            System.out.println(" Which fields want to be edit ");
+                            System.out.println("1 First Name");
+                            System.out.println("2 Last Name");
+                            System.out.println("3 Address");
+                            System.out.println("4 City");
+                            System.out.println("5 Email");
+                            System.out.println("6 Mobile");
+                            System.out.println("7 Zip");
+                            editField=sc.nextInt();
+                            switch (editField)
+                            {
+                                case 1:
+                                    sc.nextLine();
+                                    System.out.println("Enter the FirstName");
+                                    firstName=sc.nextLine();
+                                    list.get(i).setFirstName(firstName);
+                                    System.out.println("Updated first name "+firstName);
+                                    break;
+
+                                case 2:
+                                    sc.nextLine();
+                                    System.out.println("Enter the lastName");
+                                    lastName=sc.nextLine();
+                                    list.get(i).setLastName(lastName);
+                                    System.out.println("Updated last name "+lastName);
+                                    break;
+                                case 3:
+                                    sc.nextLine();
+                                    System.out.println("Enter the Address");
+                                    address=sc.nextLine();
+                                    list.get(i).setAddress(address);
+                                    System.out.println("Updated address "+address);
+                                    break;
+                                case 4:
+                                    sc.nextLine();
+                                    System.out.println("Enter the City");
+                                    city=sc.nextLine();
+                                    list.get(i).setCity(city);
+                                    System.out.println("Updated city "+city);
+                                    break;
+                                case 5:
+                                    sc.nextLine();
+                                    System.out.println("Enter the Email");
+                                    email=sc.nextLine();
+                                    list.get(i).setEmail(email);
+                                    System.out.println("Updated email "+email);
+                                    break;
+                                case 6:
+                                    sc.nextLine();
+                                    System.out.println("Enter the Mobile");
+                                    mobile=sc.nextLine();
+                                    list.get(i).setMobile(mobile);
+                                    System.out.println("Updated mobile "+mobile);
+                                    break;
+                                case 7:
+                                    sc.nextLine();
+                                    System.out.println("Enter the Pincode");
+                                    zip=sc.nextInt();
+                                    list.get(i).setZip(zip);
+                                    System.out.println("Update zip "+zip);
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    sc.nextLine();
+                    System.out.println("Enter name to be deleted ");
+                    search=sc.nextLine();
+                    for(int i=0;i< list.size();i++)
+                    {
+                        fullName=list.get(i).firstName+' '+list.get(i).lastName;
+                        if(fullName==search)
+                        {
+                            list.remove(i);
                         }
                     }
                     break;
             }
-
-        }while (choice != 5);
-
+        }while(choice!=4);
     }
 }
